bf6dec34e44d479be41f113b54b1e8654d041c9d
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2023-09-19 20:14:54 -0300

    resolvendo o desafio do modulo 2

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -1,14 +1,23 @@
 package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.services.NotaService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RetrofitInicializador {
 
+    val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
     private val retrofit = Retrofit.Builder()
+
         .baseUrl("http://192.168.1.12:8080/")
         .addConverterFactory(MoshiConverterFactory.create())
+        .client(client)
         .build()
 
     val notaService : NotaService = retrofit.create(NotaService::class.java)
